bb032862c49dc572b37ca111265e456b10ad60e8
Sarthak Agarwal <34716117+sarthak61199@users.noreply.github.com>
2021-12-10 21:56:43 +0530

    added animation

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,3 +1,4 @@
+import { motion, AnimatePresence } from "framer-motion";
 import FeedbackItem from "./FeedbackItem";
 import PropTypes from "prop-types";
 
@@ -5,15 +6,39 @@ function FeedbackList({ feedback, handleDelete }) {
   if (!feedback || feedback.length === 0) {
     return <p>No feedback</p>;
   }
+
   return (
     <div className="feedback-list">
-      {feedback.map((item) => {
-        return (
-          <FeedbackItem key={item.id} item={item} handleDelete={handleDelete} />
-        ); // Better to pass full item instead of indivisual keys of item
-      })}
+      <AnimatePresence>
+        {feedback.map((item) => {
+          return (
+            <motion.div
+              key={item.id}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+            >
+              <FeedbackItem
+                key={item.id}
+                item={item}
+                handleDelete={handleDelete}
+              />
+            </motion.div>
+          ); // Better to pass full item instead of indivisual keys of item
+        })}
+      </AnimatePresence>
     </div>
   );
+
+  // return (
+  //   <div className="feedback-list">
+  //     {feedback.map((item) => {
+  //       return (
+  //         <FeedbackItem key={item.id} item={item} handleDelete={handleDelete} />
+  //       ); // Better to pass full item instead of indivisual keys of item
+  //     })}
+  //   </div>
+  // );
 }
 
 FeedbackList.propTypes = {
