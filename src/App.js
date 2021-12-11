4976323ac6fc7021b83a61a3ec07ee419076b765
Sarthak Agarwal <34716117+sarthak61199@users.noreply.github.com>
2021-12-11 11:48:37 +0530

    added routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import AboutPageIcon from "./components/AboutPageIcon";
 import { v4 as uuidv4 } from "uuid";
 import { useState } from "react";
-import Header from "./components/Header.jsx";
+import Header from "./components/Header";
 import FeedbackData from "./data/FeedbackData";
 import FeedbackList from "./components/FeedbackList";
 import FeedbackStats from "./components/FeedbackStats";
 import FeedbackForm from "./components/FeedbackForm";
+import About from "./pages/About";
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
@@ -20,14 +23,30 @@ function App() {
   };
 
   return (
-    <>
+    <Router>
       <Header />
       <div className="container">
-        <FeedbackForm handleAdd={addFeedback} />
-        <FeedbackStats feedback={feedback} />
-        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+        <Routes>
+          <Route
+            exact
+            path="/"
+            element={
+              <>
+                <FeedbackForm handleAdd={addFeedback} />
+                <FeedbackStats feedback={feedback} />
+                <FeedbackList
+                  feedback={feedback}
+                  handleDelete={deleteFeedback}
+                />
+              </>
+            }
+          />
+
+          <Route path="/about" element={<About />} />
+        </Routes>
+        <AboutPageIcon />
       </div>
-    </>
+    </Router>
   );
 }
 
